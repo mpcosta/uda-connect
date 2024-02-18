f072faf7a758f29145cc3fd5ee4b210fd8fd13f0
mpcosta <markkcosta@gmail.com>
2024-02-18 16:55:16 +0000

    Implemented decoupled connections-api for microservice based architecture

--- modules/connections-api/app/udaconnect/__init__.py
+++ modules/connections-api/app/udaconnect/__init__.py
@@ -3,6 +3,6 @@ from app.udaconnect.schemas import ConnectionSchema  # noqa
 
 
 def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as udaconnect_api
+    from app.udaconnect.controllers import api as connections_api
 
-    api.add_namespace(udaconnect_api, path=f"/{root}")
+    api.add_namespace(connections_api, path=f"/{root}")
